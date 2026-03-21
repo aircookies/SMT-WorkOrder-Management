5580e1763cdcb73453b1d4a6c3e87e5b4efa6cbc
aircookies <aircookies@outlook.com>
2026-03-21 23:35:24 +0800

    创建项目

---
+++ backend/smt-workorder-management/src/main/java/com/aircookies/smtworkordermanagement/SmtWorkorderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.aircookies.smtworkordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtWorkorderManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmtWorkorderManagementApplication.class, args);
+    }
+
+}
---
+++ backend/smt-workorder-management/src/test/java/com/aircookies/smtworkordermanagement/SmtWorkorderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aircookies.smtworkordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtWorkorderManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
